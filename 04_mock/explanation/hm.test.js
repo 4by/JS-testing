9d33b8525c282baf9fd31e53acd866288c0c958a
idoodi <idoodi@ya.ru>
2022-04-13 17:51:18 +0300

    1

--- 04_mock/explanation/hm.test.js
+++ 04_mock/explanation/hm.test.js
@@ -1,5 +1,7 @@
 const a = require('./hm')
 const { three } = require('./hm2')
+
+
 jest.mock('./hm2')
 
 describe('one', () => {
@@ -12,7 +14,11 @@ describe('one', () => {
 
     test('shou', () => {
         jest.spyOn(a, 'two');
+        //при моке отдельных ф-й они не будут триггерится как часть других ф-й (из этого же модуля)
+        //так как тогда они "растворяются" в других 
         a.two.mockReturnValue(resp2)
+        //при моке всего модуля функции триггерятся даже в случае, если они часть других ф-й
+        //но надо задавать ответы для всех ф-й
         three.mockReturnValue(resp1)
         console.log(a.two())
         // return expect(a.one()).toEqual(our_todos)
