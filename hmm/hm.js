a6e7f026aabba2e06e8feb06b6e05dac132337a4
idoodi <idoodi@ya.ru>
2022-04-13 04:12:15 +0300

    1

--- hmm/hm.js
+++ hmm/hm.js
@@ -1,5 +1,7 @@
 const num = require('./hm2')
 
-const one = () => "hi:" + num.two()
+const three = () => "456"
+const one = () => "hi:" + num.two() + three()
 
-module.exports = { one }
+
+module.exports = { one, three }
--- hmm/hm.test.js
+++ hmm/hm.test.js
@@ -1,24 +1,21 @@
-const { one } = require('./hm')
+const { one, three } = require('./hm')
 //импортируем экшс, чтобы могли его замокать
 const { two } = require('./hm2')
 
-//сделали мок на весь модуль экшс, тем самым все его ф-и являются замоканными
-//это позволяет функционал для работы с ними 
 jest.mock('./hm2')
-//если мы хотим сделать мок только на конкретный метод модуля,
-//например на метод pow модуля Math, то следует испольщовать:
-// jest.spyOn(Math, "pow")
-
 
 describe('one', () => {
 
     beforeEach(() => {
-        our_todos = "hi:qqq"
-        response = "qqq"
+        our_todos = "hi:aaa:bbb"
+        resp1 = "aaa:"
+        resp2 = "bbb"
     })
 
-    test('should return data from backend', () => {
-        two.mockReturnValue(response)
+    test('shou', () => {
+        jest.spyOn(require('./hm'), 'three');
+        require('./hm').three.mockReturnValue(resp2)
+        two.mockReturnValue(resp1)
         return expect(one()).toEqual(our_todos)
 
     })
